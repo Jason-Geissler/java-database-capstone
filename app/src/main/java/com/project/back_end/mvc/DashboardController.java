75f9eef37dfe88d5f6d320db6a0f1d1bac0af901
Jason-Geissler <jasonbgeissler@gmail.com>
2026-02-06 17:22:02 -0600

    Update DashboardController.java

--- app/src/main/java/com/project/back_end/mvc/DashboardController.java
+++ app/src/main/java/com/project/back_end/mvc/DashboardController.java
@@ -1,6 +1,6 @@
 package com.project.back_end.mvc;
 
-import com.project.back_end.services.SharedService; // adjust to actual service package
+import com.project.back_end.services.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
